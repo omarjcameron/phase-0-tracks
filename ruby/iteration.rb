33f9eb1c7b9d79fb1e20a7fa9ad28c7eae9ef45e
Omar Cameron <omarjcameron@gmail.com>
2017-02-21 13:45:08 -0800

    Add Release 1 .each and .map

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -5,3 +5,52 @@ def take_block
 end
 
 take_block { puts "This is inside the block" }
+
+names = ["OMAR", "BRYAN", "JESS", "MATT"]
+hockey = {
+	"Crosby" => "Penguins",
+	"Ovechkin" => "Capitals",
+	"Tavares" => "Islanders"
+}
+
+# FOR ARRAY
+puts "Original Array:"
+p names
+
+names.each do |person|
+	puts "This person's name is #{person}."
+end
+
+puts "Array After .each Method:"
+p names
+
+puts "Original Array:"
+p names
+
+names.map! do |person|
+	person.downcase
+end
+
+puts "Array After .map! Method:"
+p names
+
+# FOR HASH
+puts "Original Hash:"
+p hockey
+
+hockey.each do |player, team|
+	puts "#{player} plays for the #{team}."
+end
+
+puts "Hash After .each Method:"
+p hockey
+
+puts "Original Hash:"
+p hockey
+
+new_hockey = hockey.map do |player, team|
+	team = "Islanders"
+end
+
+puts "Hash After .map Method:"
+p new_hockey
