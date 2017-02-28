516b1db16d5b80d39725e28ef7822b5b665ff298
Omar Cameron <omarjcameron@gmail.com>
2017-02-28 12:39:25 -0800

    Add puppy commands and driver code

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,32 @@ class Puppy
     toy
   end
 
+  def speak(n)
+n.times {puts "Woff"}
+  end
+
+  def roll_over
+  	"*rolls over*"
+  end
+
+  def dog_years(n)
+   dog_year = n / 7
+  end
+
+  def sit
+  	"*sits down*"
+  end
 end
 
+
+sparky = Puppy.new
+
+p sparky.fetch("ball")
+
+p sparky.speak(5)
+  
+p sparky.roll_over
+
+p sparky.dog_years(49)
+
+p sparky.sit
