80d82756f1606250219c44d489b35fe0e05d37c6
Omar Cameron <omarjcameron@gmail.com>
2017-02-28 13:11:25 -0800

    Create student class and test instance method with driver code

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -26,7 +26,7 @@ n.times {puts "Woff"}
   end
 end
 
-
+=begin
 sparky = Puppy.new
 
 p sparky.fetch("ball")
@@ -38,3 +38,34 @@ p sparky.roll_over
 p sparky.dog_years(49)
 
 p sparky.sit
+=end
+
+class Student
+
+  def initialize
+    puts "Creating student ..."
+  end
+
+  def study(subject)
+    puts "Studying hard for #{subject}!"
+  end
+
+  def report_card(gpa)
+    if gpa >= 3
+      puts "Good job!"
+    else
+      puts "Study harder!"
+    end
+  end
+end
+
+studentsArray = []
+
+50.times do
+  studentsArray << Student.new
+end
+
+studentsArray.each do |student|
+  student.study("History")
+  student.report_card(3.5)
+end
