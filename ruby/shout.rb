eada3cc43723b0336c66d289f2bdc5448b36900b
Omar Cameron <omarjcameron@gmail.com>
2017-03-02 14:59:33 -0800

    Add Release 3 to file

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,31 @@
 module Shout
 	# we'll put some methods here soon, but this code is fine for now!
-	def self.yell_angrily(words)
+	def yell_angrily(words)
 		words + "!!!" + " :("
 	end
 
-	def self.yelling_happy(words)
+	def yelling_happy(words)
 		words + "!!!" + " Oh Yea! :) "
 	end
 end
 
+=begin
 p Shout.yell_angrily("cheese")
 p Shout.yelling_happy("cheese")
+=end
+
+class The_Isley_Brothers
+	include Shout
+end
+
+
+class Coach
+	include Shout
+end
+
+
+wedding_goers = The_Isley_Brothers.new
+p wedding_goers.yelling_happy("You know you make me wanna SHOUT")
+
+coach_k = Coach.new
+p coach_k.yell_angrily("Play Defense")
