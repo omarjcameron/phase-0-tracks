e8bb294262f2f4819d9f83bf950346210f0c62cb
Omar Cameron <omarjcameron@gmail.com>
2017-03-02 14:42:07 -0800

    Add Simple Module - Release 1

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,4 +1,13 @@
 module Shout
 	# we'll put some methods here soon, but this code is fine for now!
+	def self.yell_angrily(words)
+		words + "!!!" + " :("
+	end
+
+	def self.yelling_happy(words)
+		words + "!!!" + " Oh Yea! :) "
+	end
 end
 
+p Shout.yell_angrily("cheese")
+p Shout.yelling_happy("cheese")
