351431359932dc8c2b1b7d7ad3611b671372e83c
Omar Cameron <omarjcameron@gmail.com>
2017-03-09 10:02:06 -0800

    Add two arrays (color,names) and add two new elelements to array

---
+++ js/data_structures.js
@@ -0,0 +1,11 @@
+// Declare two separate arrays:
+// One with four colors
+
+var colors = ["Blue", "Yellow", "Red", "Green"];
+var names = ["Ed", "Bert", "Scarlett", "Julie"];
+
+colors.push("Pink");
+names.push("Omar");
+
+console.log(colors);
+console.log(names);
